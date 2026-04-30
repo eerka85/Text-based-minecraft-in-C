c099c4d1c0cf955bcd00b8be047e647f867f84d0
eperka85 <johnny.stehlik@gmail.com>
2026-04-30 13:01:13 +0200

    mlib

---
+++ mlib.h
@@ -0,0 +1,43 @@
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define CYAN    "\033[36m"
+#define BOLD    "\033[1m"
+#define RESET   "\033[0m"
+#define BLUE    "\x1b[34m"
+#define GRAY	"\x1b[90m"
+#define PURPLE  "\033[35m"
+
+
+//zom//
+#define BLACK   "\033[30m"
+#define FOREST_GREEN    "\033[38;5;22m"     // A deep, dark green
+#define LIME_GREEN      "\033[38;5;118m"    // A very bright, vivid green
+
+
+//skel//
+// The deep dark voids for the eyes and mouth 
+#define SKEL_EYE_DARK    "\033[38;2;76;76;76m"   // Dark charcoal
+
+// The shading/texture colors
+#define SKEL_SHADOW      "\033[38;2;145;145;145m" // Deep shadow gray
+#define SKEL_MEDIUM      "\033[38;2;172;172;172m" // Mid-tone gray
+
+// The main "bone" colors
+#define SKEL_BONE        "\033[38;2;193;193;193m" // Standard bone gray
+#define SKEL_HIGHLIGHT   "\033[38;2;215;215;215m" // Lightest highlights
+
+
+//sheep//
+#define WHITE    "\033[38;2;255;255;255m" // Pure white highlights
+#define SHEEP_WOOL_SHADOW   "\033[38;2;230;230;230m" // Light grey wool texture
+//#define SHEEP_WOOL_DARK     "\033[38;2;213;213;213m" // Deep wool shadow - too lazy for ts rn
+
+// Face/Skin Colors
+#define SHEEP_SKIN          "\033[38;2;223;166;137m" // The main tan face color
+//#define SHEEP_SKIN_SHADOW   "\033[38;2;184;133;106m" // Darker tan for the chin/edges - too lazy for ts rn
+
+// Features
+#define SHEEP_EYE_BLACK     "\033[38;2;0;0;0m"       // Pupil
+#define SHEEP_NOSE_PINK     "\033[38;2;251;153;192m" // The pink nose/mouth area
+#define SHEEP_NOSE_LIGHT    "\033[38;2;254;206;222m" // Highlight on the nose
